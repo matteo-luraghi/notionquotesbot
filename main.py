512b9cfce4e852920b24b15fd0ae3bc3cdb3ced3
matteo-luraghi <matteoluraghi50@gmail.com>
2023-01-18 12:37:20 +0100

    adding cancel command

--- main.py
+++ main.py
@@ -168,8 +168,10 @@ def createQuote(message):
         headers = createHeaders(db[str(message.chat.id)][0])
         newPage[message.chat.id] = []
         quotesbot.send_message(message.chat.id, 'Type "Title: " followed by the title of the new page')
-        while len(newPage[message.chat.id]) != 4:
+        while len(newPage[message.chat.id]) != 4 and message.chat.id in newPage:
             if message.chat.id not in newPage:
+                return
+        if message.chat.id not in newPage:
                 return
         createUrl = "https://api.notion.com/v1/pages"
         pageName = newPage[message.chat.id][0]
