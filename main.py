53f7dc69cc89d714448d9d0c0914f53bc188b01c
matteo-luraghi <matteoluraghi50@gmail.com>
2023-11-01 11:29:46 +0100

    added type suggetstions and try catches

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def createHeaders(token: str):
         "Notion-Version": "2022-06-28"
     }
 
-def readDatabase(token, databaseId):
+def readDatabase(token : str, databaseId : str):
     headers = createHeaders(token)
     readUrl = f"https://api.notion.com/v1/databases/{databaseId}/query"
     res = requests.request("POST", readUrl, headers=headers)
@@ -35,16 +35,26 @@ def readDatabase(token, databaseId):
     data = res.json()
     quotes = []
     for el in data["results"]:
-        textObj = el["properties"]["Text"]["rich_text"]
-        text = ''
-        for phrase in textObj:
-            text += phrase["text"]["content"]
-        title = el["properties"]["Name"]["title"][0]["plain_text"]
+        try: 
+            textObj = el["properties"]["Text"]["rich_text"]
+            text = ''
+            for phrase in textObj:
+                text += phrase["text"]["content"]
+        except:
+            text = ''
+        try:
+            title = el["properties"]["Name"]["title"][0]["plain_text"]
+        except:
+            title = ''
         try:
             author = el["properties"]["Author"]["rollup"]["array"][0]["rich_text"][0]["text"]["content"]
         except:
-            author = el["properties"]["Author"]["rich_text"][0]["text"]["content"]
-        quotes.append(Quote(text, title, author))
+            try:
+                author = el["properties"]["Author"]["rich_text"][0]["text"]["content"]
+            except:
+                author = ''
+        if text != '' and title != '' and author != '':
+            quotes.append(Quote(text, title, author))
     return quotes
 
 def scheduleChecker():
@@ -54,7 +64,7 @@ def scheduleChecker():
 
 def getRandomQuote(user):
     quotes = readDatabase(user["token"], user["databaseId"])
-    if quotes != None:
+    if quotes != None and len(quotes) != 0:
         randomQuote: Quote = quotes[random.randint(0, len(quotes)-1)]
         return randomQuote
     else:
@@ -72,11 +82,11 @@ def autoQuote():
         if randomQuote != None:
             sendQuote(randomQuote, userKey)
    
-def checkToken(message):
+def checkToken(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
     if userKey in users.keys() and users[userKey]["init"] == False and "token" not in users[userKey].keys():
-        if message.text.split('_')[0] == "secret":
+        if message.text != None and message.text.split('_')[0] == "secret":
             users[userKey]["token"] = message.text
             with open("users.json", "w") as f:
                 json.dump(users, f)
@@ -87,11 +97,11 @@ def checkToken(message):
         bot.send_message(userKey, "Use the command /start to initialize the bot")
     return False
 
-def checkDatabaseId(message):
+def checkDatabaseId(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
     if userKey in users.keys() and users[userKey]["init"] == False and "databaseId" not in users[userKey].keys():
-        if len(message.text) >= 25:
+        if message.text != None and len(message.text) >= 25:
             users[userKey]["databaseId"] = message.text
             with open("users.json", "w") as f:
                 json.dump(users, f)
@@ -100,7 +110,7 @@ def checkDatabaseId(message):
     return False
 
 @bot.message_handler(commands=["start"])
-def start(message):
+def start(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
     bot.send_message(userKey, "Welcome to the Quotes Bot")
@@ -113,7 +123,7 @@ def start(message):
         bot.send_message(userKey, "Send the Notion token")
 
 @bot.message_handler(commands=["quote"])
-def quote(message):
+def quote(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
     if userKey in users.keys() and users[userKey]["init"] == True:
@@ -122,20 +132,20 @@ def quote(message):
             sendQuote(randomQuote, userKey)
 
 @bot.message_handler(commands=["help"])
-def help(message):
+def help(message : telebot.types.Message):
     with open("commands.txt", "r") as f:
         commands = f.read()
     bot.send_message(str(message.chat.id), commands)
 
 @bot.message_handler(func=checkToken)
-def verifyToken(message):
+def verifyToken(message : telebot.types.Message):
     bot.send_message(str(message.chat.id), "Send the Notion database Id")
 
 @bot.message_handler(func=checkDatabaseId)
-def verifyDatabase(message):
+def verifyDatabase(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
-    if readDatabase(users[userKey]["token"], message.text) == None:
+    if message.text != None and readDatabase(users[userKey]["token"], message.text) == None:
         del users[userKey]["token"]
         del users[userKey]["databaseId"]
         bot.send_message(userKey, "Token or Database Id not valid, send the token again")
