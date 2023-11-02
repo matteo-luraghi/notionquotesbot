f4bb977597534e2d3144429fba07d973b44d2ca2
matteo-luraghi <matteoluraghi50@gmail.com>
2023-11-02 10:43:15 +0100

    added next step handler

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ API_KEY = None
 while API_KEY == None:
     API_KEY = os.getenv("API_KEY")
 bot = telebot.TeleBot(API_KEY)
+bot.enable_save_next_step_handlers(delay=1)
+bot.load_next_step_handlers()
 
 def createHeaders(token: str) -> dict:
     return {
@@ -87,22 +89,26 @@ def autoQuote():
         if randomQuote != None:
             sendQuote(randomQuote, userKey)
    
-def checkToken(message : telebot.types.Message) -> bool:
+def checkToken(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
-    if userKey in users.keys() and users[userKey]["init"] == False and "token" not in users[userKey].keys():
-        if message.text != None and message.text.split('_')[0] == "secret":
-            users[userKey]["token"] = message.text
-            with open("users.json", "w") as f:
-                json.dump(users, f)
-            return True
-        bot.send_message(userKey, "Notion token not valid, try to send it again")
-        return False
+    if userKey in users.keys() and users[userKey]["init"] == False:
+        if "token" not in users[userKey].keys():
+            if message.text != None and message.text.split('_')[0] == "secret":
+                users[userKey]["token"] = message.text
+                with open("users.json", "w") as f:
+                    json.dump(users, f)
+                bot.send_message(userKey, "Send the Notion database ID")
+                bot.register_next_step_handler(message, checkDatabaseId)
+            else:
+                bot.send_message(userKey, "Notion token not valid, use the /start command to try again")
+        else:
+            bot.send_message(userKey, "Send the Notion database ID")
+            bot.register_next_step_handler(message, checkDatabaseId)
     elif userKey not in users.keys():
-        bot.send_message(userKey, "Use the command /start to initialize the bot")
-    return False
+        bot.send_message(userKey, "Use the /start command to initialize the bot")
 
-def checkDatabaseId(message : telebot.types.Message) -> bool:
+def checkDatabaseId(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
     if userKey in users.keys() and users[userKey]["init"] == False and "databaseId" not in users[userKey].keys():
@@ -110,22 +116,35 @@ def checkDatabaseId(message : telebot.types.Message) -> bool:
             users[userKey]["databaseId"] = message.text
             with open("users.json", "w") as f:
                 json.dump(users, f)
-            return True
-        bot.send_message(userKey, "Database Id not valid, try to send it again")
-    return False
+            if readDatabase(users[userKey]["token"], users[userKey]["databaseId"]) == None:
+                del users[userKey]["token"]
+                del users[userKey]["databaseId"]
+                with open("users.json", "w") as f:
+                    json.dump(users, f)
+                bot.send_message(userKey, "Token or Database Id not valid, use the /start command to try again")
+            else:
+                users[userKey]["init"] = True
+                bot.send_message(userKey, "Setup completed!")
+                with open("users.json", "w") as f:
+                    json.dump(users, f)
+        else:
+            bot.send_message(userKey, "Database Id not valid, use the /start command to try again")
 
 @bot.message_handler(commands=["start"])
 def start(message : telebot.types.Message):
     users = getUsers()
     userKey = str(message.chat.id)
-    bot.send_message(userKey, "Welcome to the Quotes Bot")
     if userKey not in users.keys():
+        bot.send_message(userKey, "Welcome to the Quotes Bot")
         users[userKey] = {}
         users[userKey]["init"] = False
         with open("users.json", "w") as f:
             json.dump(users, f)
     if users[userKey]["init"] == False:
         bot.send_message(userKey, "Send the Notion token")
+        bot.register_next_step_handler(message, checkToken)
+    else:
+        bot.send_message(userKey, "You are ready to use the bot!")
 
 @bot.message_handler(commands=["quote"])
 def quote(message : telebot.types.Message):
@@ -135,32 +154,15 @@ def quote(message : telebot.types.Message):
         randomQuote = getRandomQuote(users[userKey], userKey)
         if randomQuote != None:
             sendQuote(randomQuote, userKey)
+    else:
+        bot.send_message(userKey, "Use the /start command to setup the bot")
 
 @bot.message_handler(commands=["help"])
 def help(message : telebot.types.Message):
     with open("commands.txt", "r") as f:
         commands = f.read()
     bot.send_message(str(message.chat.id), commands)
-
-@bot.message_handler(func=checkToken)
-def verifyToken(message : telebot.types.Message):
-    bot.send_message(str(message.chat.id), "Send the Notion database Id")
-
-@bot.message_handler(func=checkDatabaseId)
-def verifyDatabase(message : telebot.types.Message):
-    users = getUsers()
-    userKey = str(message.chat.id)
-    if message.text != None and readDatabase(users[userKey]["token"], message.text) == None:
-        del users[userKey]["token"]
-        del users[userKey]["databaseId"]
-        bot.send_message(userKey, "Token or Database Id not valid, send the token again")
-    else:
-        users[userKey]["databaseId"] = message.text
-        users[userKey]["init"] = True
-        bot.send_message(userKey, "Setup completed!")
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-
+    
 if __name__ == "__main__":
     schedule.every().day.at(TIME).do(autoQuote)
     Thread(target=scheduleChecker).start()
